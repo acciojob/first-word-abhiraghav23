fe5f4a662ceac61771a6855d058b476250bc6737
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-28 14:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+ 	const words = s.split(' ');
+	return words[0];   
 }
 
-// Do not change the code below
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
